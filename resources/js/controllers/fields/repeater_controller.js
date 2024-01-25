69b1d70ea65e973811dc7b5db54a0348464e1431
Danil Pechenkov <d.pechenkov@rocont.ru>
2024-01-25 16:03:58 +0300

    REPEATER: фикс проблемы с tiny при  перетаскивании

--- resources/js/controllers/fields/repeater_controller.js
+++ resources/js/controllers/fields/repeater_controller.js
@@ -140,6 +140,7 @@ export default class extends ApplicationController {
             animation: 150,
             onEnd: () => {
                 this.sort();
+                this.initTiny()
             },
         });
 
@@ -247,6 +248,9 @@ export default class extends ApplicationController {
         event.currentTarget.closest('.repeater-item').remove();
 
         this.sort().checkEmpty();
+        setTimeout(() => {
+            this.initTiny();
+        }, 100);
     }
 
     /**
@@ -261,7 +265,6 @@ export default class extends ApplicationController {
         blocks.forEach((block, currentKey) => {
             block.dataset.sort = currentKey;
             const fields = block.querySelectorAll('[data-repeater-name-key]');
-
             if (!fields.length && !inputs.length) {
                 return;
             }
@@ -312,4 +315,72 @@ export default class extends ApplicationController {
             ? JSON.parse(this.data.get('ajax-data'))
             : null;
     }
+
+    initTiny() {
+        const elementsWithIdContainingText = document.querySelectorAll('.tinymce');
+        elementsWithIdContainingText.forEach((element) => {
+            const selector = "#" + element.id;
+            tinymce.init({
+                selector: selector,
+                language: 'ru',
+                plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+                toolbar: 'undo redo bold italic underline strikethrough fontfamily fontsize blocks alignleft aligncenter alignright alignjustify outdent indent  numlist bullist forecolor backcolor removeformat pagebreak charmap emoticons fullscreen code preview print insertfile image media link anchor codesample ltr rtl',
+                menubar: false,
+                //content_css: '/app/css/content-style.css',
+                //importcss_append: true,
+                table_header_type: 'section',
+                images_upload_handler: this.example_image_upload_handler
+            });
+        });
+    }
+
+    example_image_upload_handler = (blobInfo, progress) => new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        let prefix = function (path) {
+            let prefix = document.head.querySelector('meta[name="dashboard-prefix"]');
+            let pathname = `${prefix.content}${path}`.replace(/\/\/+/g, '/')
+            return `${location.protocol}//${location.hostname}${location.port ? `:${location.port}` : ''}${pathname}`;
+        };
+        let csrf_token = document.head.querySelector('meta[name="csrf_token"]').getAttribute("content");
+
+        xhr.withCredentials = false;
+        xhr.open('POST', prefix('/systems/files'));
+
+        xhr.upload.onprogress = (e) => {
+            progress(e.loaded / e.total * 100);
+        };
+
+        xhr.onload = () => {
+            if (xhr.status === 403) {
+                reject({ message: 'HTTP Error: ' + xhr.status, remove: true });
+                return;
+            }
+
+            if (xhr.status < 200 || xhr.status >= 300) {
+                reject('HTTP Error: ' + xhr.status);
+                return;
+            }
+
+            const json = JSON.parse(xhr.responseText);
+            json.location = json.relativeUrl;
+
+            if (!json || typeof json.location != 'string') {
+                reject('Invalid JSON: ' + xhr.responseText);
+                return;
+            }
+
+            resolve(json.location);
+        };
+
+        xhr.onerror = () => {
+            reject('Image upload failed due to a XHR Transport error. Code: ' + xhr.status);
+        };
+
+        const formData = new FormData();
+        formData.append('_token', csrf_token);
+        formData.append('file', blobInfo.blob(), blobInfo.filename());
+
+        xhr.send(formData);
+    });
 }
